48caeb1b5a02b7cd7cb5c3ad36067cf3c08ac565
Rocky Pukar <pukar@eaglevisionit.com>
2024-07-27 08:56:04 +0545

    fix: carousel display

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ mysqli_close($con);
     <?php include ('common/navbar.php') ?>
     <!-- Carousel Start -->
     <div class="container-fluid p-0">
-        <div class="owl-carousel header-carousel position-relative">
+        <div style="display: unset!important;" class="owl-carousel header-carousel position-relative">
             <div class="owl-carousel-item position-relative">
                 <img class="img-fluid" src="img/5.jpg" alt="">
                 <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
